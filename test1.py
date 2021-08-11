3c4a7afbd147da3cc40afdc8e85239712d74ef18
barakbanin <57184424+barakbanin@users.noreply.github.com>
2021-08-11 16:53:52 +0300

    Update test1.py

--- test1.py
+++ test1.py
@@ -1,10 +1,13 @@
 class MagicList:
-    ...:     def __init__(self):
-    ...:         self.lst = list()
-    ...:         self.counter=0
-    ...:     def __setitem__(self, i, item):
-    ...:         if len(self.lst)==0:
-    ...:             self.lst.append(item)
-    ...:             self.counter+=1
-    ...:     def __getitem__(self, i):
-    ...:         return self.lst[i]
+    def __init__(self, cls_type=None):
+        if not cls_type:
+            self.lst = list()
+        else:
+            self.lst = cls_type
+        self.counter=0
+        def __setitem__(self, i, item):
+            if len(self.lst)==0:
+                self.lst.append(item)
+                self.counter+=1
+        def __getitem__(self, i):
+            return self.lst[i]
